d0d45d431c5fb885dc45ac2290d78863baedc7ca
BenjaminLyberg <rideparanoia@gmail.com>
2016-05-10 13:16:42 +0200

    Scrapped/removed Instruments-scaffold. Added instruments to user-table

--- MusicianFinder/app/models/user.rb
+++ MusicianFinder/app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     
+    has_many :instruments
+    
     has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
     
--- MusicianFinder/db/schema.rb
+++ MusicianFinder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510100455) do
+ActiveRecord::Schema.define(version: 20160510111541) do
 
   create_table "instruments", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160510100455) do
     t.string   "location"
     t.string   "main_instrument"
     t.string   "genre"
+    t.text     "instruments"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
