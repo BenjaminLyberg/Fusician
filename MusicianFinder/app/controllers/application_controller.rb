6d8fcabfbca97571484af75f9b0115667e885111
BenjaminLyberg <rideparanoia@gmail.com>
2016-05-12 14:30:18 +0200

    Added location-option on all sites related to users

--- MusicianFinder/app/controllers/application_controller.rb
+++ MusicianFinder/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :email, :password, :image) }
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :email, :instruments, :password, :current_password, :image) }
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :image, :location) }
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :instruments, :password, :current_password, :image, :location) }
     end
 end
---
+++ MusicianFinder/app/models/location.rb
@@ -0,0 +1,3 @@
+class Location < ActiveRecord::Base
+    Cities = ['Oslo', 'Bergen', 'Stavanger']
+end
