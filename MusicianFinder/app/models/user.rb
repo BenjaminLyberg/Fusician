2a0b6b5e0b5d1aacb76a4d01eb072ab89e0dba46
BenjaminLyberg <rideparanoia@gmail.com>
2016-05-16 20:18:01 +0200

    Friend-system added
    Simple Friend-functions added (has_friendship), users friends show on
    profile. Yet to add request, accept and decline-functions.

--- MusicianFinder/app/models/user.rb
+++ MusicianFinder/app/models/user.rb
@@ -3,13 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
     
     acts_as_votable
+    has_friendship
     
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     
    
     
-    has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+    has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "80x80#" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
     
 end
