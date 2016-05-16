fc16da641e65813c6d07dfe54eba024a5cdab962
BenjaminLyberg <rideparanoia@gmail.com>
2016-05-16 19:16:35 +0200

    has_friendship added to project
    gem and migrations

--- MusicianFinder/db/schema.rb
+++ MusicianFinder/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512121858) do
+ActiveRecord::Schema.define(version: 20160516171542) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "friendable_id"
+    t.string   "friendable_type"
+    t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "blocker_id"
+    t.integer  "status"
+  end
 
   create_table "instruments", force: :cascade do |t|
     t.string   "name"
