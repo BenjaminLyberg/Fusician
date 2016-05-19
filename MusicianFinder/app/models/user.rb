f1b9c97959cd34846f4637e0c6045d0f5c4eb6f2
Preben <gunpre14@student.westerdals.no>
2016-05-19 16:47:40 +0200

    Add comments

--- MusicianFinder/app/models/user.rb
+++ MusicianFinder/app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-    
+
     acts_as_votable
     has_friendship
-    
+    has_many :comments 
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-    
-   
-    
+
+
+
     has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "80x80#" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-    
-end
 
+end
