30fa0292dbc3204c5d48a7f2cdb73176809bd37c
Preben <gunpre14@student.westerdals.no>
2016-05-26 17:57:25 +0200

    slang på .save!

--- MusicianFinder/app/controllers/subscribers_controller.rb
+++ MusicianFinder/app/controllers/subscribers_controller.rb
@@ -1,31 +1,31 @@
 class SubscribersController < ApplicationController
-    
+
     before_action :check_sub
     before_filter :authenticate_user!
-    
+
     def new
     end
-    
+
     def create
-        
+
         token = params[:stripeToken]
-        
+
         customer = Stripe::Customer.create(
             card: token,
             plan: 11,
             email: current_user.email
             )
-        
+
         current_user.subscribed = true
         current_user.stripeid = customer.id
-        current_user.save
-        
+        current_user.save!
+
         flash[:success] = "Congratulations! You are now a Premium Fusician!"
-        
+
         redirect_to root_path
-        
+
     end
-    
+
     def check_sub
         if current_user.subscribed?
             redirect_to users_profile_path
