2ecf96e75a832920b8c0567fbfacab7504a6226f
BenjaminLyberg <rideparanoia@gmail.com>
2016-05-29 17:44:29 +0200

    Welcome-mail
    Change to welcome mail

--- MusicianFinder/app/mailers/user_mailer.rb
+++ MusicianFinder/app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
      default from: 'notifications@example.com'
  
   def welcome_email(user)
+      
     @user = user
       @url  = 'fusician.herokuapp.com'
     mail(to: @user.email, subject: 'Welcome to Fusician')
