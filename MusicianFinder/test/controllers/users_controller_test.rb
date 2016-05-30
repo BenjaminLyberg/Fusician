5461a8f85b11bd44d728b71110b133ad2b74ee86
Preben <gunpre14@student.westerdals.no>
2016-05-30 19:32:00 +0200

    Begin setup for unit tests

--- MusicianFinder/test/controllers/users_controller_test.rb
+++ MusicianFinder/test/controllers/users_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user = users(:one).name
+  end
+
+  test "name should match" do
+    assert_equal 'test', @user
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+
+  test "should destroy user" do
+    assert_difference 'User.count', -1 do
+      delete :destroy, id: @user
+    end
+  end
 end
--- MusicianFinder/test/models/location_test.rb
+++ MusicianFinder/test/models/location_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get_first_location" do
+    first = Cities.top(1)
+    assert_includes first, cities(:Oslo)
+
+  end
 end
