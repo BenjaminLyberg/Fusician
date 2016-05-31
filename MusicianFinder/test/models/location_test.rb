a31a4be2adb9739a5a0df96a8ad86b2746cdb39b
Preben <gunpre14@student.westerdals.no>
2016-05-31 12:47:14 +0200

    spec init og gem file adds for testing

--- MusicianFinder/test/models/location_test.rb
+++ MusicianFinder/test/models/location_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  test "get_first_location" do
-    first = Cities.top(1)
-    assert_includes first, cities(:Oslo)
-
-  end
+  
 end
